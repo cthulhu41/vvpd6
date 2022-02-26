64cb6afc55568520edb1fec7321bfa934e7d2490
Pavel Medvedev <rufox41@gmail.com>
2022-02-26 21:38:26 +0700

    ready func2

--- main.py
+++ main.py
@@ -9,3 +9,38 @@ def is_full_connected(v, r):
 
 
 def connection_type(r, links):
+    dct = {}
+    for i in links:
+        if i[0] in dct:
+            dct[i[0]].add(i[1])
+        else:
+            dct[i[0]] = {i[1]}
+        if i[1] in dct:
+            dct[i[1]].add(i[0])
+        else:
+            dct[i[1]] = {i[0]}
+    checker = 0
+    for i in dct:
+        if len(dct[i]) == 1:
+            continue
+        elif len(dct[i]) == r:
+            checker = 1
+        else:
+            break
+    if checker:
+        return 3
+    count = 0
+    for i in dct:
+        if len(dct[i]) == 1:
+            count += 1
+        elif len(dct[i]) == 2:
+            continue
+        else:
+            return -1
+    else:
+        if count == 0:
+            return 2
+        elif count == 2:
+            return 1
+        else:
+            return -1
